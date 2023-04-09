e73aa08ca06bd96851c570ec26d8ec132ebb093c
ISNewton <ashrfalhag.11@gmail.com>
2023-04-09 09:35:42 +0200

    add loader component

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,12 +1,13 @@
 import { useSession } from "next-auth/react"
 import Link from "next/link"
+import Loader from "../loaders/loader";
 
 const Header = () => {
 
     const { data: session, status } = useSession()
 
     console.log(session, status);
-    
+
 
 
     return (
@@ -20,9 +21,11 @@ const Header = () => {
                     <span className="sr-only">Open main menu</span>
                     <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clipRule="evenodd"></path></svg>
                 </button>
+                <Loader />
                 <div className="hidden w-full md:block md:w-auto" id="navbar-default">
                     <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
                         <li>
+                           
                             <Link href={{
                                 pathname: '/',
                             }}
